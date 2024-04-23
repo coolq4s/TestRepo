54837f512ac2280fdfb9ff9f25dfac438dd29b10
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-23 23:01:31 +0700

    update ram.sh

--- Ram/ram.sh
+++ Ram/ram.sh
@@ -36,7 +36,9 @@ draw_progress_bar() {
     local num_bar=$((percent / 2))
     local num_space=$((50 - num_bar))
     printf "["
+    echo -e "\e[31m"
     printf "%0.s|" $(seq 1 $num_bar)
+    echo -e "\e[0m
     printf "%0.s-" $(seq 1 $num_space)
     printf "] %d%%\r" $percent
 }
