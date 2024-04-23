1b1f5c38735cbdfdcc8856f465192aed4f6eb9ba
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-24 01:07:06 +0700

    update ram.sh

--- Ram/ram.sh
+++ Ram/ram.sh
@@ -35,7 +35,7 @@ memtotal=$(free -m | awk "NR==2 {print \$2}")
 
 memCount $(($memused+$membuff+$memshare))
 memTotalCount=$(echo "scale=2; $memCount * 8 / 1000000" | bc)
-echo "$memTotalCount"
+echo "$memCount"
 
 # Fungsi untuk menggambar progress bar
 draw_progress_bar() {
