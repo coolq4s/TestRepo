5dd724e5402ba7e927d36c8de1e3e6b3c489661a
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-23 23:09:58 +0700

    update ram.sh

--- Ram/ram.sh
+++ Ram/ram.sh
@@ -33,8 +33,8 @@ total=100
 # Fungsi untuk menggambar progress bar
 draw_progress_bar() {
     local percent=$((progress * 100 / total))
-    local num_bar=$((percent / 2))
-    local num_space=$((50 - num_bar))
+    local num_bar=$((percent / 4))
+    local num_space=$((25 - num_bar))
     printf " ["
     printf "\e[31m%0.s|\e[0m" $(seq 1 $num_bar)
     printf "%0.s-" $(seq 1 $num_space)
