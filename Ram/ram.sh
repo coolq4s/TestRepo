6d8601c9bd5dad8d6e5c28b98ab58348e18d91e4
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-23 22:11:51 +0700

    update ram.sh

--- Ram/ram.sh
+++ Ram/ram.sh
@@ -3,10 +3,10 @@
 cleanup() {
     rm -rf ram.sh
     rm -rf TestRepo
-    echo " Cleaning up temporary files"
+    echo "Cleaning up temporary files"
     echo ""
     echo ""
-    echo -e " To try again this script,\n you can copy the command from github"
+    echo -e "To try again this script,\nyou can copy the command from github"
     echo ""
     echo ""
 }
@@ -15,29 +15,21 @@ trap cleanup EXIT
 
 clear
 
-watch -n1 -tc '
-used=$(free -m | awk "NR==2 {print \$3}" | sed "s/Mi//g; s/Gi//g; s/Ki//g")
-shared=$(free -m | awk "NR==2 {print \$5}" | sed "s/Mi//g; s/Gi//g; s/Ki//g")
-buff=$(free -m | awk "NR==2 {print \$6}" | sed "s/Mi//g; s/Gi//g; s/Ki//g")
-totaluse=$(($used+$shared+$buff))
-totalmem=$(free -h | awk "NR==2 {print \$2}" | sed "s/Mi//g; s/Gi//g; s/Ki//g")
-echo " $totaluse"
-echo " $totalmem"
+while true; do
+    used=$(free -m | awk "NR==2 {print \$3}" | sed "s/Mi//g; s/Gi//g; s/Ki//g")
+    shared=$(free -m | awk "NR==2 {print \$5}" | sed "s/Mi//g; s/Gi//g; s/Ki//g")
+    buff=$(free -m | awk "NR==2 {print \$6}" | sed "s/Mi//g; s/Gi//g; s/Ki//g")
+    totaluse=$(($used+$shared+$buff))
+    totalmem=$(free -h | awk "NR==2 {print \$2}" | sed "s/Mi//g; s/Gi//g; s/Ki//g")
 
-'
+    # Calculate percentage used
+    percentage=$(echo "scale=2; ($totaluse / $totalmem) * 100" | bc)
 
+    # Draw the bar
+    printf "Total Memory Usage: $totaluse MB / $totalmem MB\n"
+    printf "Memory Usage: %.2f%% [%.0s=]{1..%.0f}\n" $percentage $(seq 1 $(echo "$percentage / 2" | bc))
+    printf "\n"
 
-#used=$(free -h | awk 'NR==2 {print $3}' | sed "s/Mi//g; s/Gi//g; s/Ki//g")
-#shared=$(free -h | awk 'NR==2 {print $5}' | sed "s/Mi//g; s/Gi//g; s/Ki//g")
-#buff=$(free -h | awk 'NR==2 {print $6}' | sed "s/Mi//g; s/Gi//g; s/Ki//g")
-#total=$(free -h | awk 'NR==2 {print $2}' | sed "s/Mi//g; s/Gi//g; s/Ki//g")
-
-#totalused=$(($used+$shared+$buff))
-#echo -e " $totalused"
-#bbs=$(free -h | awk 'NR==2 {print $4}' | sed "s/Mi//g; s/Gi//g; s/Ki//g")
-#echo -e " $bbs"
-#free -h
-
-
-
-read -p " Press any key to continue"
+    sleep 1
+    clear
+done
