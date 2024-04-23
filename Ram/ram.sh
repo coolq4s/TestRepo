c2c09759c76ff9e856a2e79b921d171be9fcaf37
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-24 01:07:38 +0700

    update ram.sh

--- Ram/ram.sh
+++ Ram/ram.sh
@@ -33,7 +33,7 @@ membuff=$(free -w | awk "NR==2 {print \$6}")
 memshare=$(free -w | awk "NR==2 {print \$5}")
 memtotal=$(free -m | awk "NR==2 {print \$2}")
 
-memCount $(($memused+$membuff+$memshare))
+memCount=$(($memused+$membuff+$memshare))
 memTotalCount=$(echo "scale=2; $memCount * 8 / 1000000" | bc)
 echo "$memCount"
 
