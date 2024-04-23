172983d4ff7b3c96a4011fa6750b9980ec7c1b86
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-24 01:16:01 +0700

    update ram.sh

--- Ram/ram.sh
+++ Ram/ram.sh
@@ -21,7 +21,7 @@ used=$(free -w | awk "NR==2 {print \$3}")
 shared=$(free -w | awk "NR==2 {print \$5}")
 buff=$(free -w | awk "NR==2 {print \$6}")
 totalMemUsed=$(($used+$shared+$buff))
-totaluse=$($totalMemUsed * 8 / 1000000)
+totaluse=$(($totalMemUsed * 8 / 1000000))
 totalmem=$(free -h | awk "NR==2 {print \$2}")
 percentage=$(echo "scale=2; ($totaluse / $totalmem) * 100" | bc)
 
