0f05a6538352014009af9031a9a8a516a10ea960
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-23 22:26:49 +0700

    update ram.sh

--- Ram/ram.sh
+++ Ram/ram.sh
@@ -21,35 +21,12 @@ shared=$(free -m | awk "NR==2 {print \$5}" | sed "s/Mi//g; s/Gi//g; s/Ki//g")
 buff=$(free -m | awk "NR==2 {print \$6}" | sed "s/Mi//g; s/Gi//g; s/Ki//g")
 totaluse=$(($used+$shared+$buff))
 totalmem=$(free -h | awk "NR==2 {print \$2}" | sed "s/Mi//g; s/Gi//g; s/Ki//g")
-percentage=$(echo "scale=0; ($totaluse / $totalmem) * 100" | bc)
+percentage=$(echo "scale=2; ($totaluse / $totalmem) * 100" | bc)
 bar_length=$(echo "scale=0; $percentage / 2" | bc)
 echo "Total Usage: $totaluse"
 echo "Total Memory: $totalmem"
 echo "Percentage Used: $percentage%"
-
-
-# Misalkan variabel percentage memiliki nilai persentase
-
-# Hitung panjang bar
-bar_length=$((percentage / 2))
-
-# Cetak bar
-echo -n "\["
-for ((i=0; i<bar_length; i++))
-do
-    echo -n \=\
-done
-echo -n \>\
-
-# Cetak sisa bar
-for ((i=bar_length; i<50; i++))
-do
-    echo -n \ \
-done
-echo \]\
-
-echo "Persentase: $percentage%"
-'
+''
 
 
 read -p " Press any key to continue"
