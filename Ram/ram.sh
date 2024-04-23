4d1cc6ba6afd857f6de11a5ba3b60aa268a82a6f
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-23 22:49:08 +0700

    update ram.sh

--- Ram/ram.sh
+++ Ram/ram.sh
@@ -28,6 +28,31 @@ echo "Total Memory: $totalmem"
 echo "Percentage Used: $percentage%"
 
 
+
+# Inisialisasi variabel
+progress=$percentage
+total=100
+
+# Fungsi untuk menggambar progress bar
+draw_progress_bar() {
+    local percent=$((progress * 100 / total))
+    local num_bar=$((percent / 2))
+    local num_space=$((50 - num_bar))
+    printf "["
+    printf "%0.s#" $(seq 1 $num_bar)
+    printf "%0.s " $(seq 1 $num_space)
+    printf "] %d%%\r" $percent
+}
+
+# Loop untuk simulasi progres
+while [ $progress -lt $total ]; do
+    draw_progress_bar
+    ((progress += 1))
+    sleep 0.1  # simulasi pekerjaan yang sedang berlangsung
+done
+
+echo ""  # Membuat baris baru setelah progress bar selesai
+echo "Proses selesai!"
 '
 
 read -p " Press any key to continue"
