9d29626d219dff7f26d6c2bceaa718fa198ed9e5
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-23 21:32:26 +0700

    update ram.sh

--- Ram/ram.sh
+++ Ram/ram.sh
@@ -20,6 +20,9 @@ shared=$(free -h | awk 'NR==5 {print $5}' | sed "s/Mi//g; s/Gi//g; s/Ki//g")
 buff=$(free -h | awk 'NR==6 {print $6}' | sed "s/Mi//g; s/Gi//g; s/Ki//g")
 total=$(free -h | awk 'NR==2 {print $2}' | sed "s/Mi//g; s/Gi//g; s/Ki//g")
 
+totalused=$used+$shared+$buff
+echo -e " $totalused"
+
 echo -e " $total"
 
 
