100c26961c2c6f5cf9118bb8d5edbf6b3d7b7308
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-24 08:00:41 +0700

    update ram.sh

--- Ram/ram.sh
+++ Ram/ram.sh
@@ -31,11 +31,12 @@ echo "$totalMemUsed"
 
 
 if [ $totalMemUsed -ge 1024 ]; then
-    totaluse=$(echo "scale=2; $totalMemUsed / 1024" | bc) # Convert to GiB
-    echo "Total memory used in GiB: $totaluse GiB"
-else
     totaluse=$totalMemUsed # No need to convert, its already in MiB
     echo "Total memory used in MiB: $totaluse MiB"
+    
+else
+    totaluse=$(echo "scale=2; $totalMemUsed / 1024" | bc) # Convert to GiB
+    echo "Total memory used in GiB: $totaluse GiB"
 fi
 
 #totalmem=$(free -h | awk "NR==2 {print \$2}")
