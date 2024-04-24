515d924a5b8bd21ed9bbabaa2637561b163a1940
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-24 08:55:57 +0700

    update ram.sh

--- Ram/ram.sh
+++ Ram/ram.sh
@@ -38,35 +38,6 @@ fi
 
 echo "$totalresult" | sed "s/MiB//g; s/GiB//g"
 totalmem=$(free -w | awk "NR==2 {print \$2}")
-if [ $totalmem -gt 1024000 ]; then
-    totalmemInstalled=$(echo "scale=2; $totalmem / 1024 / 1024" | bc)
-    totalmemresult=$(echo $totalmemInstalled)
-    totalmemresult2=$(echo $totalmemInstalled GiB)
-
-else
-    totalmemInstalled=$(echo "scale=2; $totalmem / 1024" | bc)
-    totalmemresult=$(echo $totalmemInstalled)
-    totalmemresult2=$(echo $totalmemInstalled MiB)
-fi
-
-
-percentage=$(echo "scale=2; ($totalresult / $totalmemresult) * 100" | bc)
-
-# Inisialisasi variabel
-progress=$percentage
-total=100
-
-draw_progress_bar() {
-    local percent=$((progress * 100 / total))
-    local num_bar=$((percent / 4))
-    local num_space=$((25 - num_bar))
-    printf " RAM  ["
-    printf "\033[91m%0.s|\e[0m" $(seq 1 $num_bar)
-    printf "\033[92m%0.s-\e[0m" $(seq 1 $num_space)
-    printf "] %d%%\r" $percent
-    printf ",\033[102m\033[30m F: $memfree \033[101m\033[30m U: $memTotalCount \e[0m T: $memtotal"
-}
-draw_progress_bar
 
 '
 
