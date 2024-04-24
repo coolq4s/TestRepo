9e1009906160c2a5ff93329d5511821b7d250407
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-24 10:41:37 +0700

    update ram.sh

--- Ram/ram.sh
+++ Ram/ram.sh
@@ -140,7 +140,7 @@ draw_progress_bar_SWAP() {
 draw_progress_bar_SWAP
 echo -n "\n"
 echo -n "\n"
-' & read -p " Press any key to continue"
+' | read -p " Press any key to continue"
 
 
 #sudo sync && echo 3 > /proc/sys/vm/drop_caches
