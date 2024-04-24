8eb85fa533b7c82acb3d6f3f24bcbf1078de5830
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-24 07:52:04 +0700

    update ram.sh

--- Ram/ram.sh
+++ Ram/ram.sh
@@ -31,7 +31,7 @@ echo "$totalMemUsed"
 
 
 totaluse=$(($totalMemUsed * 8 / 10000))
-#echo " $totaluse"
+echo " $totaluse"
 #totalmem=$(free -h | awk "NR==2 {print \$2}")
 #percentage=$(echo "scale=2; ($totaluse / $totalmem) * 100" | bc)
 
