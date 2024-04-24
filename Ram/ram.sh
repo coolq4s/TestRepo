1fbd4fe33abcdba7ddb3ff7ff2205756abefdd25
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-24 08:25:09 +0700

    update ram.sh

--- Ram/ram.sh
+++ Ram/ram.sh
@@ -18,25 +18,22 @@ clear
 watch -n1 -tc '
 #RAM
 used=$(free -w | awk "NR==2 {print \$3}")
-echo "used RAM $used"
 shared=$(free -w | awk "NR==2 {print \$5}")
-echo "shared value is $shared"
 buff=$(free -w | awk "NR==2 {print \$6}")
-echo "buff value is $buff"
 cache=$(free -w | awk "NR==2 {print \$7}")
-echo "cache value is $cache"
 
 totalMemUsed=$(($used + $shared + $buff + $cache))
-echo "$totalMemUsed"
 
 
 if [ $totalMemUsed -gt 1024000 ]; then
     totaluse=$(echo "scale=2; $totalMemUsed / 1024 / 1024" | bc) # Convert to GiB
     echo "Total memory used: $totaluse GiB"
+    
 else
     totaluse=$(echo "scale=2; $totalMemUsed / 1024" | bc)
     echo "Total memory used: $totaluse MiB"
 fi
+echo "$totaluse"
 
 #totalmem=$(free -h | awk "NR==2 {print \$2}")
 #percentage=$(echo "scale=2; ($totaluse / $totalmem) * 100" | bc)
