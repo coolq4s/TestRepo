802334dc0136db2c1a66a55db020072287201508
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-24 08:16:03 +0700

    update ram.sh

--- Ram/ram.sh
+++ Ram/ram.sh
@@ -34,6 +34,7 @@ if [ $totalMemUsed -gt 1024000000 ]; then
     totaluse=$(echo "scale=2; $totalMemUsed / 1024" | bc) # Convert to GiB
     echo "Total memory used: $totaluse GiB"
 else
+    totaluse=$(echo "scale=2; $totalMemUsed / 1024" | bc)
     echo "Total memory used: $totalMemUsed MiB"
 fi
 
