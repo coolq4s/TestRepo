053b7f71f95975aa854bc4be4984adb06f209b4d
CoolQas-G <78310162+coolq4s@users.noreply.github.com>
2024-04-24 13:43:18 +0700

    update ram.sh

--- Ram/ram.sh
+++ Ram/ram.sh
@@ -5,7 +5,7 @@ cleanup() {
     rm -rf header.txt
     rm -rf TestRepo
     echo " Cleaning up temporary files"
-    echo -e " To try again this script,\n you can copy the command from github"
+    echo -e " To use again this script,\n you can copy the command from github"
     echo ""
 }
 
